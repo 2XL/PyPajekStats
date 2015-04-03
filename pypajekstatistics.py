2b50ed132150cf92955ed0d0c55caf9c53ea16a9
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-04-03 18:25:13 +0200

    Initial Commit

---
+++ pypajekstatistics.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "anna"
+__date__ = "$03/04/2015 18:17:23$"
+
+if __name__ == "__main__":
+    print "Hello World"
